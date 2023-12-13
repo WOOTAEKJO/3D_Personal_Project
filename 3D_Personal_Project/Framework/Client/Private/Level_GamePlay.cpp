309d4e7d0730464440aff3e3f595e063e6a7ec74
WOOTAEKJO <114208566+WOOTAEKJO@users.noreply.github.com>
2023-12-13 19:05:55 +0900

    실행

--- 3D_Personal_Project/Framework/Client/Private/Level_GamePlay.cpp
+++ 3D_Personal_Project/Framework/Client/Private/Level_GamePlay.cpp
@@ -80,7 +80,7 @@ HRESULT CLevel_GamePlay::Ready_Layer_Object(const wstring& strLayerTag)
 
 	CAnimMesh_Demo::ANIMDEMOVALUE AnimMeshDemoValue;
 
-	AnimMeshDemoValue.strModelTag = ANIMMODEL_JACK_TAG;
+	AnimMeshDemoValue.strModelTag = ANIMMODEL_FIONA_TAG;
 	AnimMeshDemoValue.vPos = _float4(0.f, 0.f, 0.f, 1.f);
 
 	if (FAILED(m_pGameInstance->Add_Clone(LEVEL_GAMEPLAY, strLayerTag, G0_ANIMMESH_DEMO_TAG,
--- 3D_Personal_Project/Framework/Client/Private/Loader.cpp
+++ 3D_Personal_Project/Framework/Client/Private/Loader.cpp
@@ -165,7 +165,7 @@ HRESULT CLoader::Loading_For_GamePlay_Level()
 	matPivot = XMMatrixScaling(0.01f, 0.01f, 0.01f) * XMMatrixRotationY(XMConvertToRadians(180.f));
 	//if (FAILED(m_pGameInstance->Add_ANIM_Model_ProtoType(ANIMMODEL_BAT_TAG, ANIMMODEL_BAT_PATH, matPivot))) return E_FAIL;
 	matPivot = XMMatrixScaling(0.001f, 0.001f, 0.001f) * XMMatrixRotationY(XMConvertToRadians(180.f));
-	if (FAILED(m_pGameInstance->Add_ANIM_Model_ProtoType(ANIMMODEL_JACK_TAG, ANIMMODEL_JACK_PATH, matPivot))) return E_FAIL;
+	//if (FAILED(m_pGameInstance->Add_ANIM_Model_ProtoType(ANIMMODEL_JACK_TAG, ANIMMODEL_JACK_PATH, matPivot))) return E_FAIL;
 	/*matPivot = XMMatrixScaling(0.01f, 0.01f, 0.01f) * XMMatrixRotationY(XMConvertToRadians(180.f));
 	if (FAILED(m_pGameInstance->Add_Model_ProtoType(MODEL_PINETREE_TAG, MODEL_PINETREE_PATH, matPivot))) return E_FAIL;*/
 
