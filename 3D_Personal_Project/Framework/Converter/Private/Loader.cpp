7620d97073c31421ba55d801f63f123715f944ec
WOOTAEKJO <tbxjqh1205@naver.com>
2024-01-16 04:51:38 +0900

    실행

--- 3D_Personal_Project/Framework/Converter/Private/Loader.cpp
+++ 3D_Personal_Project/Framework/Converter/Private/Loader.cpp
@@ -310,9 +310,9 @@ HRESULT CLoader::Loading_For_Converter_Level()
 //	CConverter_Model::Create(m_pDevice, m_pContext, CConverter_Model::TYPE_NONANIM, MODEL_ROCKPACK7_PATH))))
 //	return E_FAIL;
 
-if (FAILED(m_pGameInstance->Add_Component_ProtoType(m_pGameInstance->Get_Current_Level(), MODEL_SMALLROCK_TAG,
-	CConverter_Model::Create(m_pDevice, m_pContext, CConverter_Model::TYPE_NONANIM, MODEL_SMALLROCK_PATH))))
-	return E_FAIL;
+//if (FAILED(m_pGameInstance->Add_Component_ProtoType(m_pGameInstance->Get_Current_Level(), MODEL_SMALLROCK_TAG,
+//	CConverter_Model::Create(m_pDevice, m_pContext, CConverter_Model::TYPE_NONANIM, MODEL_SMALLROCK_PATH))))
+//	return E_FAIL;
 
 #pragma endregion
 
@@ -482,9 +482,9 @@ if (FAILED(m_pGameInstance->Add_Component_ProtoType(m_pGameInstance->Get_Current
 		CConverter_Model::Create(m_pDevice, m_pContext, CConverter_Model::TYPE_NONANIM, MODEL_SHOVEL_PATH))))
 		return E_FAIL;*/
 
-	if (FAILED(m_pGameInstance->Add_Component_ProtoType(m_pGameInstance->Get_Current_Level(), MODEL_HELICOBULLET_TAG,
+	/*if (FAILED(m_pGameInstance->Add_Component_ProtoType(m_pGameInstance->Get_Current_Level(), MODEL_HELICOBULLET_TAG,
 		CConverter_Model::Create(m_pDevice, m_pContext, CConverter_Model::TYPE_NONANIM, MODEL_HELICOBULLET_PATH))))
-		return E_FAIL;
+		return E_FAIL;*/
 
 
 
